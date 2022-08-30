429ac4e67bc6b2135ace9d608475bf332b8448a9
yuutom <yuuto19961204@gmail.com>
2022-08-30 23:02:00 +0900

    20220830

--- const/auth_const.py
+++ const/auth_const.py
@@ -1,6 +1,5 @@
 class AuthConstClass:
-    # gitignoreで管理
-    API_KEY = "WY2GZDQylRC3jjOJ8fzlPvuJR"
-    API_KEY_SECRET = "p9GB6J3rZ3RK8ErF1qa6oIo8rztDZJbL9Z2Vx0Ev0OkQYmLgDp"
-    ACCESS_TOKEN = "1477293137588797441-IdfcJCOIlmWVtBargyU6AQNeDyYwTj"
-    ACCESS_TOKEN_SECRET = "XrQtoiu33lt31l2qCbLuj5z5rYRyfqUla46zbwGIkT0NT"
+    API_KEY = ""
+    API_KEY_SECRET = ""
+    ACCESS_TOKEN = ""
+    ACCESS_TOKEN_SECRET = ""
